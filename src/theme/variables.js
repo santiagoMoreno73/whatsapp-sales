c4825bb7164cfb2b8d422e67a14aa1845ce0a8f0
santiago.moreno73 <santimor2009@hotmail.com>
2022-05-25 08:44:21 -0500

    [add]: navBar and globaltheme

---
+++ src/theme/variables.js
@@ -0,0 +1,23 @@
+import { css } from "styled-components";
+
+export const v = {
+  sidebarWidth: `300px`,
+  smSpacing: `8px`,
+  mdSpacing: `16px`,
+  lgSpacing: `16px`,
+  xlSpacing: `16px`,
+  xxlSpacing: `16px`,
+  borderRadius: `6px`,
+};
+
+export const btnReset = css`
+  font-family: inherit;
+  outline: none;
+  border: none;
+  background: none;
+  letter-spacing: inherit;
+  color: inherit;
+  font-size: inherit;
+  text-align: inherit;
+  padding: 0;
+`;
